4a477e76994b8a2825c34e7e0652451c21cf220a
Om-soni-15 <125734091+Om-soni-15@users.noreply.github.com>
2024-05-23 19:26:36 +0530

    capitalized titles

--- app.py
+++ app.py
@@ -9,22 +9,22 @@ import matplotlib.image
 
 st.set_page_config(layout='wide', page_title='Fashion Recommendation')
 df = pd.read_csv('df2.csv')
-st.title('fashion recommendation system')
-st.sidebar.title('select your outfits')
-gender = st.sidebar.selectbox('select gender', df['gender'].value_counts().index.to_list())
+st.title('Fashion Recommendation System')
+st.sidebar.title('Select Your Outfits')
+gender = st.sidebar.selectbox('Select Gender', df['gender'].value_counts().index.to_list())
 df = df[df['gender'] == gender]
 
 if gender:
-    season = st.sidebar.selectbox('select season', df['season'].value_counts().index.to_list())
+    season = st.sidebar.selectbox('Select Season', df['season'].value_counts().index.to_list())
     df = df[df['season'] == season]
     if season:
-        usage = st.sidebar.selectbox('select occasion', df['usage'].value_counts().index.to_list())
+        usage = st.sidebar.selectbox('Select Occasion', df['usage'].value_counts().index.to_list())
         df = df[df['usage'] == usage]
         if usage:
-            subCategory = st.sidebar.selectbox('select subCategory', df['subCategory'].value_counts().index.to_list())
+            subCategory = st.sidebar.selectbox('Select SubCategory', df['subCategory'].value_counts().index.to_list())
             df = df[df['subCategory'] == subCategory]
             if subCategory:
-                articleType = st.sidebar.selectbox('select articleType',
+                articleType = st.sidebar.selectbox('Select ArticleType',
                                                    df['articleType'].value_counts().index.to_list())
                 df = df[df['articleType'] == articleType]
 
@@ -65,7 +65,7 @@ if gender:
                                 st.write(df1['productDisplayName'].values[0])
                                 st.write('click here for virtual try on:',df1['AR LINK'].values[0])
                                 st.write('click here to purchase:', df1['Purchases link'].values[0])
-                                btn = st.button('next')
+                                btn = st.button('Next')
                                 if btn:
                                     col1, col2, col3, col4 = st.columns(4)
                                     with col1:
